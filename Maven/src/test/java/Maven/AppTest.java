14d25e83a4aa8d866a74206fc37d6d88f018f45b
miloz94 <bakamiloz@gmail.com>
2015-12-10 09:24:24 +0800

    Test added

--- Maven/src/test/java/Maven/AppTest.java
+++ Maven/src/test/java/Maven/AppTest.java
@@ -3,6 +3,14 @@ package Maven;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import g53sqm.jibble.WebServerTest;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({WebServerTest.class})
 
 /**
  * Unit test for simple App.
